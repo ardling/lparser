e92dd919baa57f4eceba9aaa7b3a07e240c4947b
ardling <ardling@localhost.localdomain>
2020-04-16 22:38:01 +0300

    add dependecies

--- setup.py
+++ setup.py
@@ -7,8 +7,15 @@ setuptools.setup(
     description="lamblda parser",
     author="Michael Ovsiannikov",
     author_email="ardling@gmail.com",
-    license="http://unlicense.org",
-    keywords="asyncio moex iss",
+    license="GPLv3",
     packages=setuptools.find_packages(exclude=["tests"]),
-    install_requires=["aiohttp"],
+    install_requires=[
+        'parsec'
+    ],
+    extras_require={
+        'testing': [
+            'pytest>=5.0.0',
+            'pytest-coverage'
+        ]
+    }
 )
