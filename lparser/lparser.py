f9eca05ca0223f049a0ef74396ce3e8b9deb1147
Michael Ovsiannikov <movsiannikov@getmobit.ru>
2020-07-15 19:30:39 +0300

    application_mod

--- lparser/lparser.py
+++ lparser/lparser.py
@@ -2,8 +2,7 @@
 
 import parsec as ps
 
-
-lmb_spaces = ps.regex(r'\s*')
+lmb_spaces = ps.regex(r'\s*')  # any number of spaces
 
 variable = ps.regex(r'[_a-zA-Z][_a-zA-Z1-9]*')
 lmb_none = ps.string('')
@@ -11,23 +10,26 @@ lmb_none = ps.string('')
 
 @ps.generate
 def app():
+    '''application: a(b c d)'''
     func = yield variable
     yield ps.string('(')
     args = yield ps.sepBy(variable, lmb_spaces)
     yield ps.string(')')
-    return (func, args)
+    return func, tuple(args)
 
 # lambda_expr = ps.string('\\') >> ps.string('.')
 
 lambda_body = app ^ variable
 
+
 @ps.generate
 def lambda_expr():
+    '''expression: \a b.b a'''
     yield ps.string('\\')
     vals = yield ps.sepBy(variable, lmb_spaces)
     yield ps.string('.')
     body = yield lambda_body
-    return vals, body
+    return tuple(vals), body
 
 
 def main():
--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -1,8 +1,13 @@
 import lparser.lparser as gr
 
 def test_1():
-    assert (gr.lambda_expr.parse('\\var1 var2.var1')) == (['var1', 'var2'], 'var1')
+    assert (gr.lambda_expr.parse('\\var1 var2.var1')) == (('var1', 'var2'), 'var1')
 
 
 def test_2():
-    assert (gr.lambda_expr.parse('\\var1 var2.var2(var3 var1)')) == (['var1', 'var2'], ('var2', ['var3', 'var1']))
+    assert (gr.lambda_expr.parse('\\var1 var2.var2(var3 var1)')) == (('var1', 'var2'), ('var2', ('var3', 'var1')))
+
+
+def _test_3():
+    assert gr.lambda_expr.parse('\\var1.\\var2.var1 var2') == (['var1'], 
+                                                                  (['var2'], ()))
